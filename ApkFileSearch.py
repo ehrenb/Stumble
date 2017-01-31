1a78326e4760192f5b32e1abeb63a74b18e69566
ehrenb <behren2@students.towson.edu>
2017-01-30 21:52:53 -0500

    more options, and extended search to use regexes

--- ApkFileSearch.py
+++ ApkFileSearch.py
@@ -1,23 +1,33 @@
 from pprint import pprint
+import re
 
 from androguard.core.bytecodes.apk import APK
 import magic
 
-a = APK('banking_malware.apk')
-#get_files_types
+class ApkFileSearch(object):
+    def __init__(self, apk_file, search_strings, file_types, case_sensitive=True):
+        self.apk = APK(apk_file)
+        self.search_strings = search_strings
+        self.file_types = file_types
 
-apk_files = a.get_files_types()
+        flags = 0
+        if not case_sensitive:
+            flags = re.IGNORECASE
+        self.patterns = [re.compile(search_string, flags=flags) for search_string in search_strings]
 
+    def search(self):
+        apk_files = self.apk.get_files_types()
+        search_results = []
+        for file_path, file_type in apk_files.iteritems():
+            if any(interested_type in file_type.lower() for interested_type in self.file_types):
+                file_data = self.apk.get_file(file_path)
 
-interested_types = ['ascii','xml']
-interested_phrases = ['http://']
-
-for file_path, file_type in apk_files.iteritems():
-    print file_type
-    if any(interested_type in file_type.lower() for interested_type in interested_types):
-        file_data = a.get_file(file_path)
-
-        if any(interested_phrase in file_data for interested_phrase in interested_phrases):
-            print file_path
-            print True
-            break
+                for pattern in self.patterns:
+                        match = pattern.search(file_data)
+                        if match:
+                            search_result = {'file_path': file_path,
+                                                      'file_type': file_type,
+                                                      'search_string': pattern.pattern,
+                                                      'match': match.group(0)}
+                            search_results.append(search_result)
+        return search_results
--- stumble.py
+++ stumble.py
@@ -1,7 +1,7 @@
 """Stumble.
 
 Usage:
-  stumble.py APK_PATHS ...  -s <SEARCH_STRING> ... [options]
+  stumble.py APK_PATHS ...  -s <SEARCH_STRING> ... [-f FILE_TYPES ...] [options]
   stumble.py (-h | --help)
   stumble.py --version
 
@@ -10,17 +10,20 @@ Arguments:
   SEARCH_STRING the string(s) you want to search for in a space seperated format
 
 Options:INPUT
-  APK_PATHS          The APK path(s) you want to search
+  APK_PATHS              The APK path(s) you want to search
   -s SEARCH_STRING  The search string(s) you want to look for
+  -f FILE_TYPES           The types of file extensions you want to search (.xml, .txt, etc...)
   --of=<out_format>  The format of the output (JSON,XML,etc)
-
+  -i                              Make search string case insensitive
 """
 
 import os 
+from pprint import pprint
 import sys
 
 from docopt import docopt
 
+from ApkFileSearch import ApkFileSearch
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='0.0.1rc')
@@ -28,6 +31,11 @@ if __name__ == '__main__':
 
     apk_paths = arguments['APK_PATHS']
     search_strings = arguments['-s']
+    file_types = arguments['-f']
+
+    file_types = [file_type.replace('.','') for file_type in file_types]#remove . from extensions
+
+    print file_types
     #file_types = arguments[]
     #save_files #save files that contain a match
     #out_file #optionally, save results to JSON
@@ -37,3 +45,7 @@ if __name__ == '__main__':
         if not os.path.isfile(path):
             print '{path} is not a valid file'.format(path=path)
             sys.exit()
+
+    for apk_path in apk_paths:
+        afs = ApkFileSearch(apk_path, search_strings, file_types)
+        pprint(afs.search())
