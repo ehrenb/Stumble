03e9ec2bdc114a17bbbd8bc8db6d5e7885854306
ehrenb <behren2@students.towson.edu>
2017-01-31 22:11:57 -0500

    added summary output file option, out_dir option, file extraction, and bulk search/extraction without search string

--- ApkFileSearch.py
+++ ApkFileSearch.py
@@ -1,3 +1,4 @@
+import os
 from pprint import pprint
 import re
 
@@ -5,13 +6,23 @@ from androguard.core.bytecodes.apk import APK
 import magic
 
 class ApkFileSearch(object):
-    def __init__(self, apk_file, search_strings, file_types, case_sensitive=True):
+    def __init__(self, 
+                      apk_file, 
+                      search_strings, 
+                      file_types, 
+                      case_insensitive=False,
+                      save_matched_files_dir=None):
         self.apk = APK(apk_file)
         self.search_strings = search_strings
         self.file_types = file_types
 
+        self.save_matched_files_dir = None
+        if save_matched_files_dir:
+            self.save_matched_files_dir = os.path.join(save_matched_files_dir, os.path.splitext(os.path.basename(apk_file))[0])
+            os.makedirs(self.save_matched_files_dir)
+
         flags = 0
-        if not case_sensitive:
+        if case_insensitive:
             flags = re.IGNORECASE
         self.patterns = [re.compile(search_string, flags=flags) for search_string in search_strings]
 
@@ -19,15 +30,38 @@ class ApkFileSearch(object):
         apk_files = self.apk.get_files_types()
         search_results = []
         for file_path, file_type in apk_files.iteritems():
-            if any(interested_type in file_type.lower() for interested_type in self.file_types):
-                file_data = self.apk.get_file(file_path)
+            file_ext = os.path.splitext(os.path.basename(file_path))[1]
+            print file_ext
+            #if file_type option on, and this file is not that type, then skip
+            if self.file_types and not any(interested_type in file_type.lower() or interested_type in file_ext for interested_type in self.file_types):
+                continue
 
-                for pattern in self.patterns:
-                        match = pattern.search(file_data)
-                        if match:
-                            search_result = {'file_path': file_path,
-                                                      'file_type': file_type,
-                                                      'search_string': pattern.pattern,
-                                                      'match': match.group(0)}
-                            search_results.append(search_result)
+            search_result = None
+
+           #record all files of specified file_type
+            if self.file_types and not self.search_strings:
+                search_result = {'file_path': file_path,
+                                           'file_type': file_type,
+                                           'search_string': None}
+                search_results.append(search_result)
+
+            file_data = self.apk.get_file(file_path)
+
+            for pattern in self.patterns:
+                    match = pattern.search(file_data)
+                    if match:
+                        search_result = {'file_path': file_path,
+                                                 'file_type': file_type,
+                                                 'search_string': pattern.pattern}
+                        search_results.append(search_result)
+
+            if search_result and self.save_matched_files_dir:
+                #save original structure to avoid duplicate filename collisions
+                save_file_path = os.path.join(self.save_matched_files_dir, file_path)
+                if not os.path.exists(os.path.dirname(save_file_path)):
+                    os.makedirs(os.path.dirname(save_file_path))
+
+                with open(save_file_path,'wb') as f:
+                    f.write(file_data)
+
         return search_results
--- stumble.py
+++ stumble.py
@@ -2,6 +2,7 @@
 
 Usage:
   stumble.py APK_PATHS ...  -s <SEARCH_STRING> ... [-f FILE_TYPES ...] [options]
+  stumble.py APK_PATHS ... -f FILE_TYPES ... [-w OUT_DIR] [-o FILE_NAME]
   stumble.py (-h | --help)
   stumble.py --version
 
@@ -9,14 +10,16 @@ Arguments:
   APK_PATHS the path of the APK you want to search through
   SEARCH_STRING the string(s) you want to search for in a space seperated format
 
-Options:INPUT
+Options:
   APK_PATHS              The APK path(s) you want to search
   -s SEARCH_STRING  The search string(s) you want to look for
   -f FILE_TYPES           The types of file extensions you want to search (.xml, .txt, etc...)
-  --of=<out_format>  The format of the output (JSON,XML,etc)
-  -i                              Make search string case insensitive
+  -i --insensitive          Make search string case insensitive
+  -o FILE_NAME           Write a result summary JSON file to FILE_NAME
+  -w OUT_DIR              Save matched files to OUT_DIR directory
 """
 
+import json
 import os 
 from pprint import pprint
 import sys
@@ -32,13 +35,16 @@ if __name__ == '__main__':
     apk_paths = arguments['APK_PATHS']
     search_strings = arguments['-s']
     file_types = arguments['-f']
+    is_case_insensitive = arguments['--insensitive']
+    output_file_name = arguments['-o']
+    save_matched_files_dir = arguments['-w']
 
     file_types = [file_type.replace('.','') for file_type in file_types]#remove . from extensions
 
     print file_types
     #file_types = arguments[]
     #save_files #save files that contain a match
-    #out_file #optionally, save results to JSON
+    #out_file #optionally, save summary results to JSON
     #
 
     for path in apk_paths:
@@ -46,6 +52,27 @@ if __name__ == '__main__':
             print '{path} is not a valid file'.format(path=path)
             sys.exit()
 
+    #initialize a file with an empty JSON array
+    if output_file_name:
+        with open(output_file_name, 'a') as f:
+            json.dump([], f)
+
     for apk_path in apk_paths:
-        afs = ApkFileSearch(apk_path, search_strings, file_types)
-        pprint(afs.search())
+        afs = ApkFileSearch(apk_path, 
+                                        search_strings,
+                                        file_types,
+                                        case_insensitive=is_case_insensitive,
+                                        save_matched_files_dir=save_matched_files_dir)
+        results = afs.search()
+        pprint(results)
+        #load existing JSON data, append, and re-dump it
+        if output_file_name and os.path.exists(output_file_name):
+            with open(output_file_name,'r') as f:
+                data = json.load(f)
+            #key as apk name for identifier
+            results = {apk_path:results}
+            data.append(results)
+            with open(output_file_name,'w') as f:
+                json.dump(data, f, sort_keys=True,
+                                             indent=4,
+                                             separators=(',',': '))
